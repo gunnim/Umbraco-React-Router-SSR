82905edd2b8d73ccaa565ee83b852fcaf2c879f7
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-04-22 21:20:42 +0000

    Umbraco passthrough routing w/ ContentFinder

---
+++ Umbraco.Extensions/ApplicationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core;
+using Umbraco.Web.Routing;
+
+namespace Umbraco.Extensions
+{
+    public class UmbEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            // Ensure our custom content finder is inserted
+            ContentFinderResolver.Current.InsertType<ContentFinder>();
+        }
+    }
+}
---
+++ Umbraco.Extensions/ContentFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Umbraco.Web;
+using Umbraco.Web.Routing;
+
+namespace Umbraco.Extensions
+{
+    /// <summary>
+    /// Handles server side routing for React Router
+    /// </summary>
+    public class ContentFinder : IContentFinder
+    {
+        public bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            var umbracoHelper = new UmbracoHelper(contentRequest.RoutingContext.UmbracoContext);
+
+            var home = umbracoHelper.ContentAtRoot().FirstOrDefault();
+
+            // Catch all requests and leave to React Router to handle
+            contentRequest.PublishedContent = umbracoHelper.TypedContent(home.Id);
+            return true;
+        }
+    }
+}
